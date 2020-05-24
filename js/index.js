dea936832afd22e2da21b66b402049e8da990e69
LuMeng9 <LuMeng9@outlook.com>
2020-05-24 13:11:32 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 $("pre").text($("pre").html());
 
 function showExample(subtitle) {
-  const htmlExams = $("ul[title=" + subtitle + "] li");
-  const examTitleArray = [];
-  for (let i = 0; i < htmlExams.length; i++) {
-    examTitleArray.push(htmlExams[i].textContent);
-    htmlExams[i].addEventlisnter();
-  }
-  console.log(examTitleArray);
+  //   $("div[class*=" + subtitle + "]").css("display", "block");
+  $("div[class*=" + subtitle + "]").show();
 }
